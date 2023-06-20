4d54e9e54404e0aa412adaeea149af45a42f2f06
nv_danya <nevzorov_daniil@mail.ru>
2023-06-21 00:24:41 +0500

    !!! ff221

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello world and!!!!!, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hello world a123123nd!!!!2!, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
